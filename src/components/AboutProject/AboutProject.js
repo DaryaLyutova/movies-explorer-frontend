fac36c9172e7f1788b58b9192857c9d27dd94cc0
Darya Lutuva <lutowa.darya@yandex.ru>
2021-02-16 16:53:23 +0300

    feat: компонент Techs, индикатор в компонент AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,29 +4,37 @@ import PageTitle from '../PageTitle/PageTitle';
 
 function AboutProject() {
     return (
-        <>
+        <section className="about-project">
             <PageTitle title={'O проекте'} />
-            <section className="about-project">
-                <ul className="about-project__items">
-                    <li className="about-project__item">
-                        <h3 className="about-project__title">Дипломный проект включал 5 этапов</h3>
-                        <p className="about-project__subtitle">Составление плана, работу над бэкендом, вёрстку, 
-                            добавление функциональности и финальные доработки.
+            <ul className="about-project__items">
+                <li className="about-project__item">
+                    <h3 className="about-project__title">Дипломный проект включал 5 этапов</h3>
+                    <p className="about-project__subtitle">Составление плана, работу над бэкендом, вёрстку,
+                    добавление функциональности и финальные доработки.
+                    </p>
+                </li>
+                <li className="about-project__item">
+                    <h3 className="about-project__title">На выполнение диплома ушло 5 недель</h3>
+                    <p className="about-project__subtitle">У каждого этапа был мягкий и жёсткий дедлайн,
+                    которые нужно было соблюдать, чтобы успешно защититься.
                         </p>
-                    </li>
-                    <li className="about-project__item">
-                        <h3 className="about-project__title">На выполнение диплома ушло 5 недель</h3>
-                        <p className="about-project__subtitle">У каждого этапа был мягкий и жёсткий дедлайн, 
-                            которые нужно было соблюдать, чтобы успешно защититься.
-                        </p>
-                    </li>
-                </ul>
-                {/* <ul className="about-project__scheme">
-                    <li></li>
-                </ul> */}
-
-            </section>
-        </>
+                </li>
+            </ul>
+            <div className="about-project__scheme">
+                <div className="about-project__scheme-item">
+                    <div className="about-project__scheme-indicator about-project__scheme-indicator_green">
+                        <p className="about-project__time">1 неделя</p>
+                    </div>
+                    <p className="about-project__type">Back-end</p>
+                </div>
+                <div className="about-project__scheme-item">
+                    <div className="about-project__scheme-indicator about-project__scheme-indicator_grey">
+                        <p className="about-project__time">4 недели</p>
+                    </div>
+                    <p className="about-project__type">Front-end</p>
+                </div>
+            </div>
+        </section>
     )
 }
 
