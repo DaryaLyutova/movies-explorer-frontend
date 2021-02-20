b0d8f5caf5f17cb7e3dffe71b76afc6a4658c819
Darya Lutuva <lutowa.darya@yandex.ru>
2021-02-20 21:59:34 +0300

    feat: компонент movies  ограничение количества карточек с фильмами

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,13 +4,16 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 
 function MoviesCardList(props) {
     return (
-        <section className="movies-cardlist">{
-            props.moviesCards.map((moviesCard) =>
+        <section className="movies-cardlist">
+        <div className="movies-cardlist__grid">{
+            props.moviesCards.slice(0, 12).map((moviesCard) =>
             (<MoviesCard
                 moviesCard={moviesCard}
             />)
             )
-        }
+        }        
+        </div>
+        <button className="movies-cardlist__button">Ещё</button>
         </section>
     )
 }
