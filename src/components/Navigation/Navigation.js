f97615805f23827138b4df5d6569389f107fb9f7
Darya Lutuva <lutowa.darya@yandex.ru>
2021-02-26 00:27:28 +0300

    Добавление стилей навигации на расширении менее 880px

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,61 +1,42 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navigation.css';
 import menLogo from '../../images/men-logo.svg';
 
 
 function Navigation(props) {
 
-    const [componentVisible, isComponentVisible] = React.useState(true);
-    const location = useLocation();
-
-    function handelHeaderTitle() {
-        if (location.pathname === '/movies'
-            || location.pathname === '/saved-movies'
-            || location.pathname === '/profile') {
-            isComponentVisible(false)
-        } else {
-            if (location.pathname === '/') {
-                isComponentVisible(true)
-            }
-        }
-    }
-
-    React.useEffect(() => {
-        handelHeaderTitle();
-    }, [location.pathname])
-
     return (
         <nav
-            className={`navigation ${!props.visible ? 'navigation-none' : ''} 
-            ${props.navOpen ? 'navigation_open' : ''}`}
-            style={{ justifyContent: componentVisible ? 'flex-end' : 'space-between' }}>
+            className={`navigation ${!props.visible ? 'navigation_none' : ''} 
+            ${props.navOpen ? 'navigation_open' : ''}`} >
+            <div className="navigation__container">
                 <button className="navigation__close navigation__button navigation__button_visible" />
-            <Link className="navigation__button navigation__button_visible">Главная</Link>
-            <ul className={`navigation__movies ${componentVisible ? 'navigation-none' : ''}`}>
-                <li>
-                    <Link
-                        to="/movies"
-                        className="navigation__button navigation__button_boild">
-                        Фильмы</Link>
-                </li>
-                <li>
-                    <Link
-                        to="/saved-movies"
-                        className="navigation__button">
-                        Сохранённые фильмы</Link>
-                </li>
-            </ul>
-            <Link
-                to="/profile"
-                className={`navigation__button-accaunt 
-                ${componentVisible ? 'navigation-none' : ''}`}>
-                Аккаунт
+                <ul className="navigation__movies">
+                    <li className="navigation__movies-item">
+                        <Link className="navigation__button navigation__button_visible">Главная</Link>
+                    </li>
+                    <li className="navigation__movies-item">
+                        <Link
+                            to="/movies"
+                            className="navigation__button navigation__button_boild">
+                            Фильмы</Link>
+                    </li>
+                    <li className="navigation__movies-item">
+                        <Link
+                            to="/saved-movies"
+                            className="navigation__button">
+                            Сохранённые фильмы</Link>
+                    </li>
+                </ul>
+                <Link to="/profile" className="navigation__button-accaunt" >
+                    Аккаунт
             <div className="navigation__button-accaunt-circle">
-                    <img src={menLogo} alt="логотип аккаунта"
-                        className="navigation-logo" />
-                </div>
-            </Link>
+                        <img src={menLogo} alt="логотип аккаунта"
+                            className="navigation-logo" />
+                    </div>
+                </Link>
+            </div>
         </nav >
     )
 }
